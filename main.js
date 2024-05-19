3b62dc27a2edb6ee532d0823c8a36fabd1c35898
Mohammed Amir <muji-amir@hotmail.com>
2024-05-19 03:26:03 -0400

    stuff

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use("/images", express.static("images"));
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -9,4 +11,4 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `<h1>Hey Dingus!</h1><img src="/kookie.png">`
+const html = `<h1>Hey Dingus!</h1><img src="/images/kookie.png">`
